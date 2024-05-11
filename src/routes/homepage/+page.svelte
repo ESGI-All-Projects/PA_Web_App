1a9f8b7a7d83225bf0fa7943472f5b53e6c32806
JulianALZ <julian.alizay@gmail.com>
2024-05-11 17:40:16 +0200

    init commit, basic front, pre deploy

---
+++ src/routes/homepage/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	// Importez tous les composants ici
+	import Header from './components/Header.svelte';
+	import BalanceSummary from './components/BalanceSummary.svelte';
+	import LineChart from './components/LineChart.svelte';
+	import ActionButtons from './components/ActionButtons.svelte';
+	import CryptoList from './components/CryptoList.svelte';
+
+	import "./homepage.css";
+</script>
+<div class="gradient-custom">
+	<Header />
+	<BalanceSummary />
+	<LineChart />
+	<ActionButtons />
+	<CryptoList />
+</div>
+
+
+
+
